b16ceffc59f4da2db65f37de331f757ec5cdaae2
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-08-29 17:00:37 -0300

    Added DegreeConverter test

---
+++ Business.Tests/DegreeConverterTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestUdemy;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class DegreeConverterTests
+    {
+        [Test]
+        public void ToFahrenheit_0Celsius_Returns32()
+        {
+            double result = DegreeConverter.ToFahrenheit(0);
+            Assert.That(result, Is.EqualTo(32));
+        }
+
+        [Test]
+        public void ToCelsius_1Fahrenheit_Returns0()
+        {
+            double result = DegreeConverter.ToCelsius(1);
+            Assert.That(result, Is.EqualTo(0));
+        }
+    }
+}
---
+++ UnitTestUdemy/DegreeConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestUdemy
+{
+    public class DegreeConverter
+    {
+        public static double ToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+
+        public static double ToCelsius(double fahrenheit) 
+        {
+            return (32 * fahrenheit - 32) * 5 / 9;
+        }
+    }
+}
