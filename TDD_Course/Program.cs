ef5ef8d4b3304d5137b6754867f8c9e7c5816072
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-09-15 17:20:25 -0300

    Adding tdd_course project

---
+++ TDD_Course/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TDD_Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Thank You Led Zeppelin"));
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Thank you led zeppelin"));
+            Console.WriteLine(NaiveCanonicalizer.GetCanonicalForm("Led zeppelin thank You"));
+
+            Console.ReadLine();
+        }
+    }
+}
