20ddfe35751cb50d510c37a508514504f687d200
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-10-04 11:38:09 -0300

    last changes

---
+++ Business.Tests/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using UnitTestUdemy;
+
+namespace Business.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void AddNumbers_ValidValues_ReturnsCorrectResult()
+        {
+            var cal = new Calculator();
+            int result = cal.AddNumbers(1, 2);
+
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }    
+}
