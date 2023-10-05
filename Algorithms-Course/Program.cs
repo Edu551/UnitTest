3912d0f7e0ca90b88df54a8f04c80517047dc6cf
Eduardo Moraes Goncalves <eduardogoncalves18@gmail.com>
2023-10-05 14:47:45 -0300

    Recursion example

--- Algorithms-Course/Program.cs
+++ Algorithms-Course/Program.cs
@@ -9,9 +9,33 @@ namespace Algorithms_Course
         {
             Console.WriteLine("Hello, World!");
 
-            TestThreeSum();
+            Console.WriteLine("Recursive = " + RecursiveFactorial(5));
+
+            Console.WriteLine(IterativeFactorial(5));
 
             Console.ReadKey();
+        }
+
+        private static int RecursiveFactorial(int n)
+        {
+            if (n == 0)
+                return 1;
+
+            return n * RecursiveFactorial(n - 1);
+        }
+
+        private static int IterativeFactorial(int number) 
+        {
+            if (number == 0)
+                return 1;
+
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            return factorial;
         }
 
         private static void TestThreeSum()
